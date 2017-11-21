cda34a1e81cfc5ec5ea49235995f643e3ad0ac25
James Calderon <jamescalderon@users.noreply.github.com>
2017-11-20 20:41:51 -0500

    Corrected schema and added connection.js (#13)
    * Development (#3)
    
    * Created basic file structure, package.json, gitignore, basic index.html (made by Moises), and started the server.js file.
    
    * Created basic file structure, package.json, gitignore, basic home.html (made by Moises), and started the server.js file.
    
    * Resubmission (corrected some files) -
     Created basic file structure, package.json, gitignore, basic home.html (made by Moises), and started the server.js file.
    
    * Added Images folder.
    
    * Filled database schema
    
    * Added fields and inserted use notes.
    
    * Added test files (will be deleted before going live), completed JSON to table mappings.
    
    * Updated schema to include foregn key on jobs table.
    
    * Updated, populated, and tested .sql files.
    
    * Added connection.js
    
    * Corrected schema conflict
    
    * Initialized sequelize.

--- model/schema.sql
+++ model/schema.sql
@@ -11,40 +11,6 @@ USE db51;
 -- URLs - refer to links to Google Docs, Dropbox, etc, as we are not storing files on our server
 -- if_company distinguishes the user type/view/access
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-	uid int NOT NULL AUTO_INCREMENT,
-=======
-<<<<<<< HEAD
-	id int NOT NULL AUTO_INCREMENT,
-=======
-	uid int NOT NULL AUTO_INCREMENT,
->>>>>>> 6b347711dab163b64c0d034110f862ab6b5e4c34
->>>>>>> Development
-	fname varchar(255) NOT NULL,
-    lname varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    pass varchar(255) NOT NULL,
-    resume_url VARCHAR(255) DEFAULT NULL, 
-    doc1_url VARCHAR(255) DEFAULT NULL, 
-    doc2_url VARCHAR(255) DEFAULT NULL,
-    doc3_url VARCHAR(255) DEFAULT NULL,
-    if_company BOOLEAN DEFAULT false,
-<<<<<<< HEAD
-	PRIMARY KEY (uid)
-);
-
-=======
-<<<<<<< HEAD
-    
-	PRIMARY KEY (id)
-);
-
-
-
-=======
-	PRIMARY KEY (uid)
-=======
 create table users (
 	uid INT NOT NULL AUTO_INCREMENT,
 	first_name VARCHAR(255),
@@ -62,47 +28,12 @@ create table users (
 	doc2 LONGTEXT,
 	doc3 LONGTEXT,
   PRIMARY KEY (uid)
->>>>>>> 8161b79faa1865cd79e9d794afc54854b0566807
 );
 
->>>>>>> 6b347711dab163b64c0d034110f862ab6b5e4c34
->>>>>>> Development
 -- table which holds jobs applied for
 CREATE TABLE jobs
 (
 -- Notes:
-<<<<<<< HEAD
--- Anything that starts with hr is specific to the hiring company's actions
--- Field Mapping: To the right is the JSON object fields, the first array element results (with 0 showing), and the data types  in parentheses.
--- db table created in same order of JSON responce (see test.json)
-=======
-<<<<<<< HEAD
--- anything that starts with jobs is specific to the listing
--- anything that starts with hr is specific to the hiring company's actions
->>>>>>> Development
-
-  jid INT NOT NULL AUTO_INCREMENT,  
-  job_title VARCHAR(255) NOT NULL, -- listings.listing[0].title (string)
-  job_desc LONGTEXT DEFAULT NULL, -- listings.listing[0].description (string - HTML)
-  job_posted VARCHAR(255) DEFAULT NULL, -- listings.listing[0].post_date (string - Date/Time)
-  if_remote INT DEFAULT 0, -- listings.listing[0].telecommuting (int - boolean)
-  job_type_id VARCHAR(10), -- listings.listing[0].category.id (int)
-  job_type_name VARCHAR(255), -- listings.listing[0].category.name (string)
-  if_ft INT DEFAULT 1, -- listings.listing[0].type.id (boolean int)
-  comp_name VARCHAR(255), -- listings.listing[0].comp.name (string)
-  comp_url VARCHAR(255), -- listings.listing[0].comp.url (string - HTML)
-  comp_loc VARCHAR(255), -- listings.listing[0].comp.location.name
-  comp_logo VARCHAR(255), -- listings.listing[0].company.logo (string - HTML)
-  apply_url VARCHAR(255), -- listings.listing[0].apply_url (string - HTML)
-  job_url VARCHAR(255), -- listings.listing[0].url (string - HTML)
-  hr_feedback LONGTEXT DEFAULT NULL,
-<<<<<<< HEAD
-=======
-   hr_if_hired BOOLEAN DEFAULT false,
-  
-  PRIMARY KEY (id)
-);
-=======
 -- Anything that starts with hr is specific to the hiring company's actions
 -- Field Mapping: To the right is the JSON object fields, the first array element results (with 0 showing), and the data types  in parentheses.
 -- db table created in same order of JSON responce (see test.json)
@@ -122,7 +53,6 @@ CREATE TABLE jobs
   apply_url VARCHAR(255), -- listings.listing[0].apply_url (string - HTML)
   job_url VARCHAR(255), -- listings.listing[0].url (string - HTML)
   hr_feedback LONGTEXT DEFAULT NULL,
->>>>>>> Development
   hr_if_hired BOOLEAN DEFAULT false,
   uid int, -- foreign key pointing to user ID
   PRIMARY KEY (jid),
@@ -131,7 +61,3 @@ CREATE TABLE jobs
 
 
 -- api call example: see test.json
-<<<<<<< HEAD
-=======
->>>>>>> 6b347711dab163b64c0d034110f862ab6b5e4c34
->>>>>>> Development
