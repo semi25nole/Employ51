dbd4df4b96fb7276302adc50454cd36ca5c5535a
jamescalderon <j.e.calderon@gmail.com>
2017-11-20 19:55:07 -0500

    Initialized sequelize.

--- model/connection.js
+++ model/connection.js
@@ -14,5 +14,14 @@ var sequelize = new Sequelize("db51", "root", pass, {
     }
 });
 
+//Checking connection status
+sequelize.authenticate().complete(function(err) {
+    if (err) {
+        console.log('There is connection in ERROR: ' + err);
+    } else {
+        console.log('Connection has been established successfully');
+    }
+});
+
 // Exports the connection for other files to use
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -19,4 +19,12 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Static directory
 app.use(express.static("public"));
 
-//routes
+// Routes
+// =============================================================
+require("./app/routes/api-routes.js")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
