d57652e67c89393352100a971493e89572f554e8
jamescalderon <j.e.calderon@gmail.com>
2017-11-24 13:24:52 -0500

    Add routes for gener pages.

--- control/routing/fileRoutes.js
+++ control/routing/fileRoutes.js
@@ -1,10 +1,24 @@
 var path = require("path");
 module.exports = function(app) {
 
-
+    //routes:
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../../view/public/home.html"));
+        res.sendFile(path.join(__dirname, "../../view/public/home.html")); //base page
     });
+
+    app.get("/hr", function(req, res) {
+        res.sendFile(path.join(__dirname, "../../view/public/HR.html"));
+    });
+
+    app.get("/user", function(req, res) {
+        res.sendFile(path.join(__dirname, "../../view/public/user.html"));
+    });
+
+    app.get("/test", function(req, res) {
+        res.sendFile(path.join(__dirname, "../../view/public/test.html")); //TODO: remove before going live
+    });
+
+
 
     // app.get("/survey", function(req, res) {
     //     res.sendFile(path.join(__dirname, ""));
