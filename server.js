660610cabe04569d22fc393710f2b12143aefc1b
jamescalderon <j.e.calderon@gmail.com>
2017-11-25 18:52:57 -0500

    Tweaked user and job models to associate correctly. Since Sequelize runs it's own schema actions (based on models), removed schema.sql from project.

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
@@ -16,17 +14,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Static directory
-app.use(express.static("view/public"));
 
-// Routes
-require("./control/routing/file_routes.js")(app);
+app.use(express.static("view/public")); // Static directory
+
+//Routes
+require("./control/routing/file_routes.js")(app); //page routes
 require("./control/routing/user_routes.js")(app);
 require("./control/routing/job_routes.js")(app);
 
-// Starts the server to begin listening
-
 db.sequelize.sync({ force: true }).then(function() {
+    // Starts the server to begin listening
     app.listen(PORT, function() {
         console.log("Employ51 App listening on PORT " + PORT);
     });
