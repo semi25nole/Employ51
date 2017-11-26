607deca539111284456cdcd0e162281743f568f3
Kyle Ruschel <dkr1985@icloud.com>
2017-11-25 19:43:45 -0500

    Basic setup (#33)
    * Development (#3)
    
    * Created basic file structure, package.json, gitignore, basic index.html (made by Moises), and started the server.js file.
    
    * Created basic file structure, package.json, gitignore, basic home.html (made by Moises), and started the server.js file.
    
    * Resubmission (corrected some files) -
     Created basic file structure, package.json, gitignore, basic home.html (made by Moises), and started the server.js file.
    
    * Added Images folder.
    
    * Filled database schema
    
    * Added fields and inserted use notes.
    
    * Added test files (will be deleted before going live), completed JSON to table mappings.
    
    * Updated schema to include foregn key on jobs table.
    
    * Updated, populated, and tested .sql files.
    
    * Added connection.js
    
    * Corrected schema conflict
    
    * Initialized sequelize.
    
    * Updated some elements in Home.css for possible drop shadow and removed clock from SVG background (can be re-added by uncommenting XML section labeled 'clock').
    
    * Put JS back at bottom of page (oops!)
    
    * emove connection.js file (we have config.json for Sequelize), update config.json to use db51 (Sequelize test passed*), for clarity changed name of /model to /db as there was also a /models folder (necessary for Sequelize).
    
    * Added Sequelize models.
    
    * Added description to Readme.md
    
    * Work on bugs, sequelize running, and confirmed base route working.
    
    * Correct Express static directory mapiing and corrected local css/js path to match.
    
    * Add routes for gener pages.
    
    * Rename app.js to home.js (each page should have their own js file matching name), added /home file route in addition to / file route (both point to home.html), updated users seed in seed.sql.
    
    * Updated entire seed.sql to work with new sequelize format (start the server app, then seed database, as Sequelize destroys existing db every time server app run).
    
    * Rename route files for different actions.
    
    * Tweaked user and job models to associate correctly. Since Sequelize runs it's own schema actions (based on models), removed schema.sql from project.
    
    * Added user get route.

--- models/jobs.js
+++ models/jobs.js
@@ -1,7 +1,100 @@
 //jobs table model
 
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('jobs', {
+    var Job = sequelize.define('Job', {
+        jid: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        job_title: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        job_desc: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        job_posted: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        if_remote: {
+            type: DataTypes.INTEGER(11),
+            allowNull: true,
+            defaultValue: '0'
+        },
+        job_type_id: {
+            type: DataTypes.STRING(10),
+            allowNull: true
+        },
+        job_type_name: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        if_ft: {
+            type: DataTypes.INTEGER(11),
+            allowNull: true,
+            defaultValue: '1'
+        },
+        comp_name: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        comp_url: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        comp_loc: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        comp_logo: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        apply_url: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        job_url: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        hr_feedback: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        hr_if_hired: {
+            type: DataTypes.INTEGER(1),
+            allowNull: true,
+            defaultValue: '0'
+        }
+    }, {
+        tableName: 'jobs'
+    });
+
+    Job.associate = function(models) {
+        // We're saying that a Job should belong to an User
+        // A Job can't be created without an User due to the foreign key constraint
+        Job.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Job;
+};
+
+
+/*
+ BACKUP:
+//jobs table model
+
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('Job', {
         jid: {
             type: DataTypes.INTEGER(11),
             allowNull: false,
@@ -83,3 +176,7 @@ module.exports = function(sequelize, DataTypes) {
         tableName: 'jobs'
     });
 };
+
+
+
+*/
