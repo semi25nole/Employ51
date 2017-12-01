77625d3af5c6effb3dc22ff2904425bd739e7d39
James Calderon <jamescalderon@users.noreply.github.com>
2017-12-01 10:02:19 -0500

    Search Route Tested/Made (#61)
    * Added test files (will be deleted before going live), completed JSON to table mappings.
    
    * Updated schema to include foregn key on jobs table.
    
    * Updated, populated, and tested .sql files.
    
    * Added connection.js
    
    * Corrected schema conflict
    
    * Initialized sequelize.
    
    * Updated some elements in Home.css for possible drop shadow and removed clock from SVG background (can be re-added by uncommenting XML section labeled 'clock').
    
    * Put JS back at bottom of page (oops!)
    
    * emove connection.js file (we have config.json for Sequelize), update config.json to use db51 (Sequelize test passed*), for clarity changed name of /model to /db as there was also a /models folder (necessary for Sequelize).
    
    * Added Sequelize models.
    
    * Added description to Readme.md
    
    * Work on bugs, sequelize running, and confirmed base route working.
    
    * Correct Express static directory mapiing and corrected local css/js path to match.
    
    * Add routes for gener pages.
    
    * Rename app.js to home.js (each page should have their own js file matching name), added /home file route in addition to / file route (both point to home.html), updated users seed in seed.sql.
    
    * Updated entire seed.sql to work with new sequelize format (start the server app, then seed database, as Sequelize destroys existing db every time server app run).
    
    * Rename route files for different actions.
    
    * Tweaked user and job models to associate correctly. Since Sequelize runs it's own schema actions (based on models), removed schema.sql from project.
    
    * Added user get route.
    
    * Implemented, tested, and updated seed.sql to re-seed db on new server app run.
    
    * Added back schema.sql to be used as a mapping reference.
    
    * updates
    
    * Completed and tested user auth route.
    
    * Updated .gitignore
    
    * Completed display route.
    
    * Cleaned up, reimplemented, and tested test.js to help see objects for middleware.
    
    * Added separate test files and routes for Kyle and James.
    
    * Cleaned up some code.
    
    * Created and tested Maint Create User Route
    
    * Updated and tested User Get (read) route
    
    * Updated notes
    
    * Implemented user update route
    
    * Implemented User delete. Updated seed.sql
    
    * Updated gitignore to ignore the config.json file so everyone can use their own password for their local db
    
    * Updated gitignore
    
    * Replaced 'maint' with 'user' in routes, as it was more appropriate.
    
    * Finished routes: Implemented and tested job CRUD routes.
    
    * Tests copied to Kyle test html as well.
    
    * uxui upload
    
    * Implemented search in TEST file - had to make route and implement CORS package to not get back localhost restrictions.
    
    * Search functionality and example added to test files

---
+++ view/public/assets/js/search.js
@@ -0,0 +1,33 @@
+var request = require("request");
+
+function mySearch(search, location) {
+    // this.apiKey = 'eef2ebd00509e1f156de2fe3ea5065c8';
+    // this.method = 'aj.jobs.search';
+    // this.perpage = 15;
+    // this.format = 'json';
+    this.query = search || 'developer'; //default value
+    this.place = location || 'remote';
+
+    var URL = 'https://authenticjobs.com/api/?api_key=eef2ebd00509e1f156de2fe3ea5065c8&method=aj.jobs.search&perpage=15&format=json&query=' + this.query + '&location=' + this.place;
+
+    // Then run a request to the OMDB API with the movie specified
+    request(URL, function(error, response, body) {
+
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            // console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+            return JSON.parse(body);
+        }
+    });
+
+
+
+}
+
+
+
+
+module.exports = mySearch;
