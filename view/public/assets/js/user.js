5c8ad7ba35b508c7b0d146ce02f2f9b6f62526ca
James Calderon <jamescalderon@users.noreply.github.com>
2017-12-01 23:23:39 -0500

    User Page (#69)
    * Added test files (will be deleted before going live), completed JSON to table mappings.
    
    * Updated schema to include foregn key on jobs table.
    
    * Updated, populated, and tested .sql files.
    
    * Added connection.js
    
    * Corrected schema conflict
    
    * Initialized sequelize.
    
    * Updated some elements in Home.css for possible drop shadow and removed clock from SVG background (can be re-added by uncommenting XML section labeled 'clock').
    
    * Put JS back at bottom of page (oops!)
    
    * emove connection.js file (we have config.json for Sequelize), update config.json to use db51 (Sequelize test passed*), for clarity changed name of /model to /db as there was also a /models folder (necessary for Sequelize).
    
    * Added Sequelize models.
    
    * Added description to Readme.md
    
    * Work on bugs, sequelize running, and confirmed base route working.
    
    * Correct Express static directory mapiing and corrected local css/js path to match.
    
    * Add routes for gener pages.
    
    * Rename app.js to home.js (each page should have their own js file matching name), added /home file route in addition to / file route (both point to home.html), updated users seed in seed.sql.
    
    * Updated entire seed.sql to work with new sequelize format (start the server app, then seed database, as Sequelize destroys existing db every time server app run).
    
    * Rename route files for different actions.
    
    * Tweaked user and job models to associate correctly. Since Sequelize runs it's own schema actions (based on models), removed schema.sql from project.
    
    * Added user get route.
    
    * Implemented, tested, and updated seed.sql to re-seed db on new server app run.
    
    * Added back schema.sql to be used as a mapping reference.
    
    * updates
    
    * Completed and tested user auth route.
    
    * Updated .gitignore
    
    * Completed display route.
    
    * Cleaned up, reimplemented, and tested test.js to help see objects for middleware.
    
    * Added separate test files and routes for Kyle and James.
    
    * Cleaned up some code.
    
    * Created and tested Maint Create User Route
    
    * Updated and tested User Get (read) route
    
    * Updated notes
    
    * Implemented user update route
    
    * Implemented User delete. Updated seed.sql
    
    * Updated gitignore to ignore the config.json file so everyone can use their own password for their local db
    
    * Updated gitignore
    
    * Replaced 'maint' with 'user' in routes, as it was more appropriate.
    
    * Finished routes: Implemented and tested job CRUD routes.
    
    * Tests copied to Kyle test html as well.
    
    * uxui upload
    
    * Implemented search in TEST file - had to make route and implement CORS package to not get back localhost restrictions.
    
    * Search functionality and example added to test files
    
    * Cleaned up code and added error response to search route.
    
    * Implemented modals on user page, id in URL, moved jQuery to user.js, created a redirect id id not present in URL, commented out Applications link, since we have Saved Jobs,
    
    * Corrected jQuery reference and adjusted job route
    
    * User Implemented

--- view/public/assets/js/user.js
+++ view/public/assets/js/user.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function() { //begin jQuery load page
     var url = $(this).attr('data-url');
     var current_url = window.location.href;
     var uid = 0;
-
+    var jobObject;
 
     var substring = 'id=';
 
     if (current_url.indexOf(substring) !== -1) {
-        var array = current_url.split('id=');
-        uid = parseInt(array[1]);
+        var ary = current_url.split('id=');
+        uid = parseInt(ary[1]);
     } else {
         window.location.href = "/"; //redirect to home
     }
@@ -19,25 +19,53 @@ $(document).ready(function() {
     //     $('#applicationModal').modal('show');
     // });
 
-    $('#resumes').on('click', function() {
+    $('#del_yes').on('click', function() {
+        window.location.href = "/"; //redirect to home
+    });
+
+    $('#resumeOptions').on('click', function() {
+        addUserDocstoModal();
         $('#resumeModal').modal('show');
     });
 
-    $('#uploadMyResume').on('click', function() {
-        $('#uploadResume').modal('show');
-    });
+    // $('#uploadMyResume').on('click', function() {
+    //     $('#uploadResume').modal('show');
+    // });
 
     $('#savedJobs').on('click', function() {
-        //modify modal
-        showUserandJobs(uid);
-
-
-
-
-
-
-        $('#savedJobModal').modal('show');
+        addUserJobstoModal(uid); //fill modal with job results
+        $('#savedJobModal').modal('show'); //show
     });
+
+    //delete chosen job(s) on modal
+
+    var chosenModalJob = $('select[name=selector]').val(); //$('#mySavedJobs_select').val();
+    console.log(chosenModalJob);
+
+    function delUserJobstoModal(idIn) {
+        var displayVar = {
+            id: idIn
+        };
+        event.preventDefault(); //crucial to see correct results!
+
+        $.post("/api/display", displayVar, function(data) {
+
+            var array = data[0].Jobs;
+
+            for (var index = 0; index < array.length; index++) {
+                var title = array[index].job_title;
+                var comp = array[index].comp_name;
+                var jID = array[index].id;
+
+                $('#mySavedJobs_select').append($('<option>', {
+                    value: jID,
+                    text: title + ' at ' + comp
+
+                }));
+            }
+
+        });
+    }
 
     $('#deleteAccount').on('click', function() {
         $('#deleteModal').modal('show');
@@ -58,23 +86,116 @@ $(document).ready(function() {
 
 
 
+
     //custom functions
     //tests user/hr 'full results (job and user info)
-    function showUserandJobs(idIn) {
+    function addUserJobstoModal(idIn) {
         var displayVar = {
             id: idIn
         };
         event.preventDefault(); //crucial to see correct results!
-        console.log('sent:');
-        console.log(displayVar);
+
         $.post("/api/display", displayVar, function(data) {
-            console.log('received back:');
             console.log(data);
-            var myJSON = JSON.stringify(data);
-            $('#r').text(myJSON);
-        }, "json");
+            var array = data[0].Jobs;
+
+            for (var index = 0; index < array.length; index++) {
+                var title = array[index].job_title;
+                var comp = array[index].comp_name;
+                var jID = array[index].id;
+
+                $('#mySavedJobs_select').append($('<option>', {
+                    value: jID,
+                    text: title + ' at ' + comp
+                }));
+            }
+
+        });
+    }
+
+    function deleteAccnt(idIn) {
+        var displayVar = {
+            id: idIn
+        };
+        event.preventDefault(); //crucial to see correct results!
+
+        $.post("/api/display", displayVar, function(data) {
+            console.log(data);
+            var array = data[0].Jobs;
+
+            for (var index = 0; index < array.length; index++) {
+                var title = array[index].job_title;
+                var comp = array[index].comp_name;
+                var jID = array[index].id;
+
+                $('#mySavedJobs_select').append($('<option>', {
+                    value: jID,
+                    text: title + ' at ' + comp
+                }));
+            }
+
+        });
+
+
+    }
+
+    //tests individual user/hr info from user table
+    function apiUser_Delete(idIn) {
+        event.preventDefault(); //crucial to see correct results!
+        // var listItemData = $(this).parent("td").parent("tr").data("author");
+        // var id = listItemData.id;
+
+        //can use above or an object/var(?):
+        // var id = 2;
+        console.log('sent:');
+        console.log(id);
+        $.ajax({
+                method: "DELETE",
+                url: "/api/user/" + idIn,
+                dataType: "json"
+            })
+            .done(function(data) {
+                console.log('received back:');
+                console.log(data);
+                var myJSON = JSON.stringify(data);
+                $('#r').text(myJSON);
+            });
+    }
+
+    function addUserDocstoModal(idIn) {
+
+        $('#myResume_select').append($('<option>', {
+            value: 0,
+            text: 'Document 1'
+        }));
+
+        $('#myResume_select').append($('<option>', {
+            value: 1,
+            text: 'Document 2'
+        }));
+
+        $('#myResume_select').append($('<option>', {
+            value: 2,
+            text: 'Document 3'
+        }));
+
+        // var displayVar = {
+        //     id: idIn
+        // };
+        // event.preventDefault(); //crucial to see correct results!
+
+        // $.post("/api/display", displayVar, function(data) { 
+
+
+
+
+
+
+
+
+        // });
     }
 
 
-    console.log(uid);
-});
+
+}); //end jQuery load page
